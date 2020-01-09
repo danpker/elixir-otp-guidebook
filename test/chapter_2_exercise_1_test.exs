083f0135bbd946a9c83bbdf7da870b12d42dffef
Daniel Parker <danpker@gmail.com>
2020-01-09 12:28:41 +0000

    Add extra tests for exercise 1

--- test/chapter_2_exercise_1_test.exs
+++ test/chapter_2_exercise_1_test.exs
@@ -4,4 +4,12 @@ defmodule Chapter2Exercise1Test do
   test "sums all elements in list" do
     assert Chapter2.Exercise1.sum([1,2,3]) == 6
   end
+
+  test "empty list will be 0" do
+    assert Chapter2.Exercise1.sum([]) == 0
+  end
+
+  test "simple element list will be the same" do
+    assert Chapter2.Exercise1.sum([5]) == 5
+  end
 end
